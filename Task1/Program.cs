d373d978de5d54de9236aea06541efa9c6d28124
Sergey Grozov <grozoff92@gmail.com>
2021-02-18 10:09:06 +0500

    Задача 1 готово

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,9 +4,66 @@ namespace Task1
 {
     class Program
     {
+        public class TestCase
+        {
+            public int Input { get; set; }
+            public string Expected { get; set; }
+        }
+        static string JustNumb(int n)
+        {
+            int d = 0;
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                }
+                i++;
+            }
+
+            string actual;
+            if (d == 0)
+            {
+                Console.WriteLine(actual = "Простое");
+            }
+            else
+            {
+                Console.WriteLine(actual = "Не простое");
+            }
+
+            return actual;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var testData = new TestCase[4];
+            testData[0] = new TestCase()
+            {
+                Input = 2,
+                Expected = "Простое"
+            };
+            testData[1] = new TestCase()
+            {
+                Input = 57,
+                Expected = "Не простое"
+            };
+            testData[2] = new TestCase()
+            {
+                Input = 181,
+                Expected = "Простое"
+            };
+            testData[3] = new TestCase()
+            {
+                Input = 132,
+                Expected = "Не простое"
+            };
+
+            foreach (var testCase in testData)
+            {
+                var result = JustNumb(testCase.Input);
+                var correct = result == testCase.Expected;
+                Console.WriteLine($"Number {testCase.Input}, Answer {correct}");
+            }
         }
     }
 }
